00dc2f34eda85d8adecbb772e1b15cf51eab122d
AbdullahElhammar <abdallah15102000abdo@gmail.com>
2024-04-13 23:03:49 +0200

    add to new repo

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NETCore_Lecture3.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public async  Task<IActionResult> add(int id, string name, int age,IFormFile stdimg)
+        {
+            string FileExt = stdimg.FileName.Split('.').Last();
+            //var fs = new FileStream($"wwwroot/images/file1.{FileExt}", FileMode.Create);
+            //await stdimg.CopyToAsync(fs);
+            //fs file is created by OS and to release this file to be able to used by other users:
+            //fs.Dispose();
+
+            //another and the model use to use Dispose();
+            using (var fs = new FileStream($"wwwroot/images/file1.{FileExt}", FileMode.Create))
+            {
+                stdimg.CopyToAsync(fs);   //used only used with dispoible objects
+            }
+
+            return Content("added");
+        }
+
+
+    }
+}
---
+++ Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NETCore_Lecture3.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            int id = 10;
+            string name = "abdullah";
+            Response.Cookies.Append("id",id.ToString());
+            Response.Cookies.Append("name",name);
+            return Content("data added in cooky");
+        }
+        public IActionResult Show()
+        {
+            string name = Request.Cookies["name"];
+            int id=int.Parse (Request.Cookies["id"]);
+            return Content($"{name}, {id}");
+
+        }
+    }
+}
